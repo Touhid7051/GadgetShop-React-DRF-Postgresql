8cd4bd61fbdee50598ecd29d8e0c3b5a5b7b0881
Touhid7051 <touhidul15-7051@diu.edu.bd>
2021-03-12 16:56:04 +0600

    add to cart functionality in cartScreen

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { CART_ADD_ITEM } from '../constants/cartConstants'
 
 export const addToCart = (id, qty) => async (dispatch, getState)=>{
-    const {data} =await axios.get(`/api/products/${id}`)
+    const {data} =await axios.get(`http://127.0.0.1:8000/api/products/${id}`)
     
     dispatch({
         type:CART_ADD_ITEM,
