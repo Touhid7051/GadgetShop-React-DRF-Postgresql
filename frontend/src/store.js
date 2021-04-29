855fbf017be109ceaae061a85171d86d0092f02e
touhid <touhidul15-7051@diu.edu.bd>
2021-04-30 02:49:14 +0600

    Shipping Address Screen & Save Address

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -21,10 +21,16 @@ const cartItemsFromStorage = localStorage.getItem('cartItems') ?
     
 const userInfoFromStorage = localStorage.getItem('userInfo') ? 
     JSON.parse(localStorage.getItem('userInfo')) : null
+    
+const shippingAddressFromStorage = localStorage.getItem('shippingAddress') ? 
+    JSON.parse(localStorage.getItem('sgippingAddress')) : {}
 
 const intitialState = {
-    cart:{ cartItems: cartItemsFromStorage },
-    userLogin:{userInfo: userInfoFromStorage}
+    cart:{ 
+          cartItems: cartItemsFromStorage,
+          shippingAddress: shippingAddressFromStorage,
+        },
+    userLogin:{userInfo: userInfoFromStorage},
 }
 
 const middleware = [thunk]
