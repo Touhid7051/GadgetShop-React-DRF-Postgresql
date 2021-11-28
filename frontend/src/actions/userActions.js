6d20e4805bc82758317c3c6d7896a8c370aee7b5
Touhid7051 <touhidul15-7051@diu.edu.bd>
2021-11-28 19:04:56 +0600

    User List for admin in Frontend

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -18,6 +18,11 @@ import { USER_LOGIN_REQUEST,
     USER_UPDATE_PROFILE_SUCCESS,
     USER_UPDATE_PROFILE_FAIL,
     USER_UPDATE_PROFILE_RESET,
+    
+    USER_LIST_REQUEST,
+    USER_LIST_SUCCESS,
+    USER_LIST_FAIL,
+    USER_LIST_RESET,
     } from '../constants/userConstants'
 
 import { ORDER_LIST_MY_RESET } from '../constants/orderConstants'
@@ -65,6 +70,7 @@ export const logout = () => (dispatch) => {
     dispatch({type : USER_LOGOUT})
     dispatch({type : USER_DETAILS_RESET})
     dispatch({type : ORDER_LIST_MY_RESET})
+    dispatch({type : USER_LIST_RESET})
 }
 
 
@@ -187,6 +193,45 @@ export const updateUserProfile = (user) => async (dispatch, getState) => {
     }catch(error){
         dispatch({
             type:USER_UPDATE_PROFILE_FAIL,
+            payload:error.response && error.response.data.detail
+            ? error.response.data.detail
+            :error.message,
+        })
+    }
+}
+
+
+export const listUsers = (user) => async (dispatch, getState) => {
+    try{
+        dispatch({
+            type:USER_LIST_REQUEST
+        })
+        
+        const {
+            userLogin: { userInfo },
+        } = getState()
+        
+        const config={
+            headers:{
+                'Content-type': 'application/json',
+                Authorization : `Bearer ${userInfo.token}` //Authorization Token 
+            }
+        }
+        
+        const {data} = await axios.get(
+            'http://127.0.0.1:8000/api/users/',
+            config,
+            )
+        
+        dispatch({
+            type:USER_LIST_SUCCESS,
+            payload:data
+        })
+                
+        
+    }catch(error){
+        dispatch({
+            type:USER_LIST_FAIL,
             payload:error.response && error.response.data.detail
             ? error.response.data.detail
             :error.message,
